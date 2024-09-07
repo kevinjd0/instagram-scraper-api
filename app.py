f2691fa15793a808c4f0a2087a18c3371e1ffd24
Abhinav K K <abhinavkk1562003@gmail.com>
2024-09-08 00:40:06 +0400

    Add new get data route and / route

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 import instaloader
 from flask_cors import CORS
+import requests
+import time
+import threading
 
 app = Flask(__name__)
 
@@ -63,6 +66,67 @@ def get_profile_bio_posts():
 
     except Exception as e:
         return jsonify({"error": str(e)})
+
+
+def delete_file_after_delay(filepath, delay):
+    time.sleep(delay)
+    try:
+        if os.path.exists(filepath):
+            os.remove(filepath)
+            print(f"File {filepath} deleted.")
+    except Exception as e:
+        print(f"Error deleting file: {e}")
+
+
+@app.route('/fetch_instagram_profile', methods=['GET'])
+def fetch_instagram_profile():
+    username = request.args.get('username')
+    if not username:
+        return jsonify({"error": "username parameter is missing"}), 400
+
+    try:
+        # Construct the URL and headers
+        url = "https://i.instagram.com/api/v1/users/web_profile_info/?username=" + username
+
+        payload = {}
+        headers = {
+            'x-ig-app-id': '936619743392459',
+        }
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+        # Define a local directory to store the downloaded profile picture
+        local_directory = 'profile_pics'
+        if not os.path.exists(local_directory):
+            os.makedirs(local_directory)
+
+        # Save the profile picture locally
+        # print(response.json())
+        # profile_pic_url = response.json().data.user.profile_pic_url
+
+        # get profile pic from response
+        profile_pic_url = response.json()["data"]["user"]["profile_pic_url"]
+
+        local_filename = os.path.join(
+            local_directory, f"{username}_profile_pic")
+        L.download_pic(local_filename, profile_pic_url, 0)
+
+        # Start a thread to delete the file after 10 seconds
+        threading.Thread(target=delete_file_after_delay,
+                         args=(local_filename, 10)).start()
+
+        # If the request is successful, return the JSON response
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return jsonify({"error": f"Failed to fetch profile info. Status code: {response.status_code}"}), response.status_code
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
+    return send_from_directory('profile_pics', filename)
 
 
 if __name__ == '__main__':
